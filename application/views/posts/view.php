3b53abed9847c2e52f03533dd9bfbd21bf290e11
piyush kushwah <kushwahpiyush4@gmail.com>
2018-02-12 22:03:39 +0530

    posts edit functionality added

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,12 +1,14 @@
 <h2>
 <?php echo $post['title'];?>
 </h2>
-<small>Posted on:<?php echo $post['created_at'];?></small>
+<small >Posted on:<?php echo $post['created_at'];?></small>
 <div class='post-body'>
 <?php echo $post['body']?>
 </div>
 
 <hr>
+
 <?php echo form_open('posts/delete/'.$post['id']);?>
+<a class="btn btn-success pull-left" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">EDIT</a>
 <input type='submit' value='Delete' class='btn btn-danger'>
 </form>
