3d84d99b2cba2e874502de7b77ca062411596f43
piyush kushwah <kushwahpiyush4@gmail.com>
2018-02-12 10:37:48 +0530

    create posts

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -23,10 +23,17 @@ class Posts extends CI_Controller{
         }
         public function create(){
             $data['title'] = 'Create Post';
+            $this->form_validation->set_rules('title','Title','required');
+            $this->form_validation->set_rules('body','Body','required');
 
+            if($this->form_validation->run()===FALSE){
             $this->load->view('templates/header');
             $this->load->view('posts/create',$data);
             $this->load->view('templates/footer');
+            }else {
+                $this->post_model->create_post();
+              redirect('posts');
+            }
 
         }
 }
--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,4 +1,5 @@
 <h2><?= $title?></h2>
+<?php echo validation_errors();?>
 <?php echo form_open('posts/create'); ?>
   <div class="form-group">
     <label >Title</label>
@@ -6,7 +7,7 @@
   </div>
   <div class="form-group">
     <label >Body</label>
-    <textarea  class="form-control"  placeholder="Add Body"></textarea>
+    <textarea name='body' class="form-control"  placeholder="Add Body" ></textarea>
   </div>
     <button type="submit" class="btn btn-default">Submit</button>
 </form>
